97eb313a010cfa5b7618a615d06f1032f03936fa
nvte6 <nathanieljap@gmail.com>
2020-08-06 20:56:57 -0400

    changing response

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-app.get('/', (req, res)=> {res.send(database.users)});
+app.get('/', (req, res)=> {res.send('it is working!')});
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
 app.get('/profile/:id', profile.handleProfileGet(db));
